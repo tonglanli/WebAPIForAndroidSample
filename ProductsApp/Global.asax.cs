364b42bd9fd0706af5b465b9048151c9a3d33a50
tonglanli <lanli.tong@hp.com>
2013-11-14 20:04:00 +0800

    Initial

---
+++ ProductsApp/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ProductsApp
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            ConfigureApi(GlobalConfiguration.Configuration);
+        }
+
+        void ConfigureApi(HttpConfiguration config)
+        {
+            // Remove the JSON formatter
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            // or
+
+            // Remove the XML formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
